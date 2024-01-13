d04fac46834b48f7838df9530fd5ebcd3f47583e
Catalin Rizea <111205627+marcatriz@users.noreply.github.com>
2024-01-13 12:04:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OCR_FE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/scripts.js
@@ -0,0 +1,6 @@
+﻿document.getElementById('upload-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    var fileInput = document.getElementById('id-file');
+    var file = fileInput.files[0];
+    // Add AJAX request to send the file to your backend
+});
